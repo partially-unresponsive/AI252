f53847e7b8f03b5aae21a36ee65173c0e906f476
CutieSai <unsecuredmail@proton.me>
2025-11-20 12:55:14 +0200

    TP: Lab 5 cryptography start

---
+++ TP/Lab5.py
@@ -0,0 +1,40 @@
+from izzyutils.batch1.meowwers import constrained_sum_sample_pos
+
+import numpy as np
+import math 
+import datetime 
+import os 
+import cryptography as crypt 
+import string 
+
+# NOTE: Taken directly from test_attendance.py
+def hex_token(n, total):
+  """Turns the constrained_sum_sample_pos numbers into hex."""
+  return [hex(i) for i in constrained_sum_sample_pos(n, total)]
+
+
+def hex_to_dec(n):
+  return int(n, 16)
+
+
+def hex_to_dec_list(lst):
+  return [int(i, 16) for i in lst]
+
+
+def main():
+  sample = hex_token(3, 6969420)
+  print(f"{sample} = {sum(hex_to_dec_list(sample))}")
+  # print(sum(constrained_sum_sample_pos(4, 6969420)));
+  print(hex_to_dec('0x43'))
+  from cryptography.fernet import Fernet, MultiFernet
+  key1 = Fernet(Fernet.generate_key())
+  key2 = Fernet(Fernet.generate_key())
+  f = MultiFernet([key1, key2])
+  token = f.encrypt(b"Meowwers nya~")
+  print(f"Token: {token}\nDecrypted: {f.decrypt(token)}")
+
+
+if __name__ == "__main__":
+  import doctest
+  main()
+  doctest.testmod()
